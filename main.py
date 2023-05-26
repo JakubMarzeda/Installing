72c83c954e77e07b7277c14cfa04b424bd01c35c
JakubMarzeda <jakub.marzeda11@gmail.com>
2023-05-26 23:46:33 +0200

    Add decorator on get to display name

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 def root(request: Request):
     return {"Jakub Marzęda": "Aplikacja do nauki słówek z języka angielskiego"}
+
+
+@app.get("/name")
+def get_name(request: Request):
+    return {"Name": request}
 
 
 if __name__ == "__main__":
