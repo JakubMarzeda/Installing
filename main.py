b235707992793d7d3bbd6cf8ffd4bb643060c6ec
JakubMarzeda <jakub.marzeda11@gmail.com>
2023-05-30 11:04:49 +0200

    Test 1

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI, Request
 import uvicorn
-
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def root(request: Request):
-    return {"Jakub Marzęda": "Aplikacja do nauki słówek z języka angielskiego"}
+    return templates.TemplateResponse("main_page.html", {"request": request})
 
-
-@app.get("/name")
-def get_name(request: Request):
-    return {"Name": request}
-
+@app.post("/")
+async def get_word(request: Request):
+    form = await request.form()
+    word = form.get("word")
+    print(word)
 
 if __name__ == "__main__":
     uvicorn.run(app)
